c29701c3bdd64b326b62fc9d39822d6fa9596235
Malus <hallo89@protonmail.com>
2024-04-26 15:31:50 +0200

    Improve ycm_extra_conf.py with varying usefulness
    The javascript formatting options don't work, I think

--- .vim/ycm_extra_conf.py
+++ .vim/ycm_extra_conf.py
@@ -1,8 +1,24 @@
 def Settings( **kwargs ):
+  if kwargs[ 'language' ] == 'javascript':
+    return {
+      'ls': {
+        'javascript.format.insertSpaceAfterOpeningAndBeforeClosingNonemptyBraces': True,
+        'javascript.format.insertSpaceAfterOpeningAndBeforeClosingNonemptyBrackets': True,
+        'javascript.format.insertSpaceAfterOpeningAndBeforeClosingNonemptyParenthesis': True,
+
+        'typescript.format.insertSpaceAfterOpeningAndBeforeClosingNonemptyBraces': True,
+        'typescript.format.insertSpaceAfterOpeningAndBeforeClosingNonemptyBrackets': True,
+        'typescript.format.insertSpaceAfterOpeningAndBeforeClosingNonemptyParenthesis': True,
+      }
+    }
   if kwargs[ 'language' ] == 'deno':
     return {
       'ls': {
         'enable': True,
-        'lint': True
+        'lint': True,
+        'suggest': {
+          'completeFunctionCalls': True,
+          'autoImports': True
+        }
       }
     }
